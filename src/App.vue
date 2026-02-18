95e86097155c4ee0a40ca9153c4e22258aebd21d
ksach <183852846+KSACH114@users.noreply.github.com>
2026-02-18 16:00:01 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TopBar />
+  </div>
+
+</template>
+
+<script setup>
+  import TopBar from './components/TopBar.vue'
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,21 @@
+
+
+<template>
+<li>
+  <a href="/">主页</a>
+  <a href="/">文章</a>
+  <a href="/">关于</a>
+  <!-- <a href="/"></a> -->
+</li>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+li {
+  display: inline-block;
+  margin-right: 10px;
+}
+</style>
